738c987cc78897d1d820ce17d6ec247b9fae8337
Krzysztof Białek <kbialek@users.noreply.github.com>
2025-12-29 17:49:32 +0100

    Update version_added metadata property
    Signed-off-by: Krzysztof Białek <kbialek@users.noreply.github.com>

--- plugins/modules/apk.py
+++ plugins/modules/apk.py
@@ -11,7 +11,7 @@ description:
   - The M(community.openwrt.apk) module manages packages on OpenWrt using the apk package manager (available in OpenWrt 25.12+).
   - It can install and remove packages.
 author: Krzysztof Bialek (@kbialek)
-version_added: 0.3.0
+version_added: 0.4.0
 extends_documentation_fragment:
   - community.openwrt.attributes
 attributes:
