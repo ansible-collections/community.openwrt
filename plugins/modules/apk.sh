a40a3b7f8c349b3b4bb99cf5ae87ca6097bc0606
Krzysztof Białek <kbialek@users.noreply.github.com>
2026-01-02 09:28:02 +0100

    Add shellcheck code blocks
    Signed-off-by: Krzysztof Białek <kbialek@users.noreply.github.com>

--- plugins/modules/apk.sh
+++ plugins/modules/apk.sh
@@ -11,7 +11,9 @@ query_package() {
 install_packages() {
     local _IFS pkg pkgs_to_install
     # shellcheck disable=SC2154,SC2086
-    _IFS="$IFS"; IFS=","; set -- $name; IFS="$_IFS"
+    {
+        _IFS="$IFS"; IFS=","; set -- $name; IFS="$_IFS"
+    }
     
     for pkg; do
         query_package "$pkg" || pkgs_to_install="$pkgs_to_install $pkg"
@@ -36,7 +38,9 @@ install_packages() {
 remove_packages() {
     local _IFS pkg pkgs_to_remove
     # shellcheck disable=SC2154,SC2086
-    _IFS="$IFS"; IFS=","; set -- $name; IFS="$_IFS"
+    {
+        _IFS="$IFS"; IFS=","; set -- $name; IFS="$_IFS"
+    }
 
     for pkg; do
         query_package "$pkg" && pkgs_to_remove="$pkgs_to_remove $pkg"
