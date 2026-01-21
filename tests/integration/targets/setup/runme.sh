68b19463d93732be32ddd93ff0aa52ee5b9c6dbd
Alexei Znamensky <russoz@gmail.com>
2026-01-22 00:53:40 +1300

    rename common script file

--- tests/integration/targets/setup/runme.sh
+++ tests/integration/targets/setup/runme.sh
@@ -6,4 +6,4 @@
 export SCRIPT_DIR
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-exec "$OUTPUT_DIR/../../../utils/runme.sh" "$(basename "$SCRIPT_DIR")" "$@"
+exec "$OUTPUT_DIR/../../../utils/integration-common-runme.sh" "$(basename "$SCRIPT_DIR")" "$@"
