6a016c88fe8bb2250aef0b4aed35f24df3169dac
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2026-01-22 08:14:14 +1300

    test(integration): refactor runme.sh, remove pytest dependency (#103)
    * test(integration): refactor runme.sh, remove pytest dependency
    
    * rename common script file

--- tests/integration/targets/ping/runme.sh
+++ tests/integration/targets/ping/runme.sh
@@ -3,23 +3,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-[ "${DEBUG:-}" != "" ] && set -x
-
-export CLI_VERBOSITY="${1:-}"
-export OPENWRT_VERSION="${OPENWRT_VERSION:-24.10.4}"
-
+export SCRIPT_DIR
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-REPO_ROOT="$(cd "$OUTPUT_DIR/../../../../" && pwd)"
 
-# shellcheck disable=SC2155
-export TEST_TARGET_NAME="$(basename "$SCRIPT_DIR")"
-
-source virtualenv.sh
-pip install molecule 'molecule-plugins[docker]'
-[ -x /usr/bin/docker ] || {
-    sudo apt-get update && sudo apt-get install -y docker.io
-}
-
-# shellcheck disable=SC2164
-cd "$REPO_ROOT"
-pytest -s tests/utils/integration/tests
+exec "$OUTPUT_DIR/../../../utils/integration-common-runme.sh" "$(basename "$SCRIPT_DIR")" "$@"
