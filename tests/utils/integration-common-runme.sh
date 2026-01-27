5d2ce631b7cf3b60c0653fb75d785d2f1402e5c7
Alexei Znamensky <russoz@gmail.com>
2026-01-27 16:37:43 +1300

    chore: adjust license tags in multiple files

--- tests/utils/integration-common-runme.sh
+++ tests/utils/integration-common-runme.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Copyright (c) 2026, Alexei Znamensky (@russoz)
-# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 [ "${DEBUG:-}" != "" ] && set -x
