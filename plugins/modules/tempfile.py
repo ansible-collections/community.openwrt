e2d781fd64108fa90992afe93d2c3b764eb092d4
Ilya Bogdanov <zeerayne1337@gmail.com>
2026-02-04 21:02:50 +0300

    feat: add `tempfile` module

---
+++ plugins/modules/tempfile.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# GNU General Public License v3.0 (see https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import annotations
+
+
+DOCUMENTATION = r"""
+module: tempfile
+short_description: Creates temporary files and directories on OpenWrt nodes
+description:
+  - The M(community.openwrt.tempfile) module creates temporary files and directories on the OpenWrt target.
+author: Ilya Bogdanov (@zeerayne)
+extends_documentation_fragment:
+  - community.openwrt.attributes
+  - community.openwrt.attributes.info_module
+options:
+  state:
+    description:
+      - Whether to create a file or a directory.
+    type: str
+    choices: [ file, directory ]
+    default: file
+  path:
+    description:
+      - Location where the temporary file or directory should be created.
+      - If path is not specified, the default system temporary directory (C(/tmp)) will be used.
+    type: path
+    default: /tmp
+  prefix:
+    description:
+      - Prefix of the file or directory name.
+      - The default prefix is C(ansible).
+    type: str
+    default: ansible
+"""
+
+EXAMPLES = r"""
+- name: Create temporary file
+  community.openwrt.tempfile:
+    state: file
+  register: temp_file_1
+
+- name: Create temporary directory with `build` prefix
+  community.openwrt.tempfile:
+    state: directory
+    prefix: build
+  register: temp_dir_1
+"""
+
+RETURN = r"""
+path:
+  description: The absolute path to the created file or directory.
+  returned: success
+  type: str
+  sample: /tmp/ansible.bMlvdk
+"""
