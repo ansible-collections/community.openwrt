6b44e492f9bd10265e3aa155a944da5526c80623
Alexei Znamensky <russoz@gmail.com>
2026-02-22 17:52:15 +1300

    fix(modules/setup): return empty dict instead of skipping

--- plugins/modules/setup.sh
+++ plugins/modules/setup.sh
@@ -9,8 +9,11 @@ add_ubus_fact() {
     set -- ${1//!/ }
     ubus list "$2" > /dev/null 2>&1 || return
     local json="$($ubus call "$2" "$3" 2>/dev/null)"
-    [ -n "$json" ] || return
-    echo -n "$delimiter\"$1\":$json"
+    if [ -n "$json" ]; then
+        echo -n "$delimiter\"$1\":$json"
+    else
+        echo -n "$delimiter\"$1\":{}"
+    fi
     delimiter=","
 }
 
