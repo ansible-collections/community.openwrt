5e76a6dce66b01fc1518e6cd422e0731dd68a310
Alexei Znamensky <russoz@gmail.com>
2026-05-17 11:09:19 +1200

    chore: file formatting

--- plugins/_facts/gather_facts.py
+++ plugins/_facts/gather_facts.py
@@ -9,29 +9,28 @@ import importlib.util
 import os
 from unittest import mock
 
-from ansible import constants as C
 import ansible.plugins.action as _ansible_action_pkg
+from ansible import constants as C
 
 # Import the builtin gather_facts ActionModule by file path to avoid a circular
 # import: Ansible loads this file under the module name
 # 'ansible.plugins.action.gather_facts', so a regular import of that name would
 # resolve back to this very file.
-_builtin_path = os.path.join(os.path.dirname(_ansible_action_pkg.__file__), 'gather_facts.py')
-_spec = importlib.util.spec_from_file_location('_ansible_builtin_gather_facts', _builtin_path)
+_builtin_path = os.path.join(os.path.dirname(_ansible_action_pkg.__file__), "gather_facts.py")
+_spec = importlib.util.spec_from_file_location("_ansible_builtin_gather_facts", _builtin_path)
 _builtin_module = importlib.util.module_from_spec(_spec)
 _spec.loader.exec_module(_builtin_module)
 GatherFactsActionModule = _builtin_module.ActionModule
 
-OPENWRT_SETUP = 'community.openwrt.setup'
+OPENWRT_SETUP = "community.openwrt.setup"
 
 
 class ActionModule(GatherFactsActionModule):
-
     def run(self, tmp=None, task_vars=None):
         if task_vars is None:
             task_vars = {}
 
-        if not task_vars.get('openwrt_gather_facts'):
+        if not task_vars.get("openwrt_gather_facts"):
             return super().run(tmp, task_vars)
 
         _orig_execute_module = self._execute_module
@@ -52,5 +51,5 @@ class ActionModule(GatherFactsActionModule):
                 return setup_action.run(task_vars=task_vars)
             return _orig_execute_module(module_name=module_name, module_args=module_args, task_vars=task_vars, **kwargs)
 
-        with mock.patch.object(self, '_execute_module', _patched_execute_module):
+        with mock.patch.object(self, "_execute_module", _patched_execute_module):
             return super().run(tmp, task_vars)
